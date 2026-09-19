2984b3a44f4159a337f580784c746727e89afbd4
nakodesk-tech <nakodesk@gmail.com>
2026-09-19 10:28:27 +0530

    Bump app version to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nakodesk.eduaillocal"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     buildFeatures { compose = true }

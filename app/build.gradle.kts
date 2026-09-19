743e28ed15ac52b149fef758978e8a09b59053fc
nakodesk-tech <nakodesk@gmail.com>
2026-09-19 10:07:56 +0530

    Set app release version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.nakodesk.eduaillocal"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildFeatures { compose = true }
